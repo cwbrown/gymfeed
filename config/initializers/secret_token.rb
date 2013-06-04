7f364a7184d7915af6987140de1bad021a194f6d
Christopher Brown <cbrown@mathbook\032pro.home>
2013-06-04 13:14:23 -0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gymfeed::Application.config.secret_key_base = 'fe38fa0feadb01f2cdd7d8119c59616934681c25081240f17eec15c9c6d2b9b356dc27f5ae3000509455734375f82cf55a24e9c9f83bb190773069cc3f235338'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gymfeed::Application.config.session_store :cookie_store, key: '_gymfeed_session'
