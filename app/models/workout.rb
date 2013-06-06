319835bb94d7982ebcce9246fe10a548bac3b41b
Chris Brown <brown.chrisw@gmail.com>
2013-06-06 14:51:45 -0600

    Fixed typos and Devise with Rails 4.0 issues (attr_accesible)

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,6 +3,6 @@ class Workout
   field :name, type: String
   field :program, type: String
   field :description, type: String
-  embedded_in: gymday
-  embeds_many: scores
+  embedded_in :gymday
+  embeds_many :scores
 end
